41b8a2e25ee75864d1dcf1c1b815997cb9ca2df6
Dhirendra <yadavdhirendra93@gmail.com>
2024-10-02 00:10:24 +0530

    order service commit

--- orderservice/orderservice/src/main/java/com/orderservice/controller/OrderController.java
+++ orderservice/orderservice/src/main/java/com/orderservice/controller/OrderController.java
@@ -4,6 +4,8 @@ package com.orderservice.controller;
 import com.basedomain.dto.Order;
 import com.basedomain.dto.OrderEvent;
 import com.orderservice.kafka.OrderProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1")
 public class OrderController {
+    public static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     private OrderProducer orderProducer;
     public OrderController(OrderProducer orderProducer) {
@@ -23,6 +26,7 @@ public class OrderController {
 
     @PostMapping("/orders")
     public String placeOrder(@RequestBody Order order) {
+        logger.info("This is order service");
         order.setOrderId(UUID.randomUUID().toString());
 
         OrderEvent orderEvent = new OrderEvent();
